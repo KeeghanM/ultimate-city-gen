fa117db63e0eefd0a33d9b584fdda78fa45beab2
Keeghan McGarry <keeghan_m@live.com>
2023-01-29 14:29:00 +0000

    Moved opening of panes to own file

--- sketch.js
+++ sketch.js
@@ -205,31 +205,7 @@ function createUiElements() {
   detail_pane_container.position(0, 100)
 
   btn_city_detail = createButton("🌆")
-  btn_city_detail.mouseClicked(() => {
-    let pane_type = "city_details"
-    // if it's already open, close it
-    for (let pane of panes) {
-      if (pane.type == pane_type) {
-        pane.destroy()
-      }
-    }
-
-    panes.push(
-      new Pane({
-        type: pane_type,
-        name: town_name.value(),
-        x: 25,
-        y: 25,
-        components: [
-          {
-            label: "Population",
-            type: "text",
-            value: Math.round(buildings.length * 2.5).toString(), // TODO: Switch to "GetPopulation()"
-          },
-        ],
-      })
-    )
-  })
+  btn_city_detail.mouseClicked(openCityDetail)
 
   btn_district_detail = createButton("🚧")
 
